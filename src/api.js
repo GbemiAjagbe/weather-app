984486a3daac6819bf801b1eb51adef9c133f90d
GbemiAjagbe <dajagbe357@stu.ui.edu.ng>
2023-01-17 04:12:26 -0600

    Finished App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5f0d56f159msh49d152baa0084ecp18eff8jsne27c7728da20",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "2362deac931c0ffd8b9f01cc22d98b0e"
